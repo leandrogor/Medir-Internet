45cf6a495581647e1f078fe87e6f6e5907f65708
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-06-21 13:24:41 -0300

    Update empaquetar.bat

--- empaquetar.bat
+++ empaquetar.bat
@@ -1,3 +1,3 @@
 for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set "date=%%G"
-set "filename=MedirInternet-%date:~6,2%-%date:~4,2%-%date:~0,4%.zip"
-7z a "%filename%" medirInternet.py startMedirInternet.bat
+set filename=MedirInternet-%date:~6,2%-%date:~4,2%-%date:~0,4%.zip
+7z a %filename% medirInternet.py startMedirInternet.bat
