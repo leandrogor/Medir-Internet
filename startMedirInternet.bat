63de70fe28aee35eed5f7fcc78e5431ca144cbd4
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-06-21 09:26:30 -0300

    Update startMedirInternet.bat

--- startMedirInternet.bat
+++ startMedirInternet.bat
@@ -3,7 +3,7 @@
 REM Verificar si Python está instalado
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python no está instalado.
+    echo Python no instalado.
     REM Abrir la página de detalles de Python 3.11 en la Microsoft Store
     start ms-windows-store://pdp/?ProductId=9NRWMJP3717K
     echo Por favor, instala Python desde Microsoft Store y luego vuelve a ejecutar este archivo.
