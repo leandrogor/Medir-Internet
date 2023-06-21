e268ac2e27bb239bdc3f4148f3562449474e082f
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-06-21 16:14:15 -0300

    Update startMedirInternet.bat

--- startMedirInternet.bat
+++ startMedirInternet.bat
@@ -1,27 +1,28 @@
 @echo off
 setlocal enabledelayedexpansion
 
-
-REM Verificar si Python está instalado
+REM Verificar si Python esta instalado
 python --version >nul 2>&1
+
 if !errorlevel! neq 0 (
     echo Python no instalado.
-	
+
     REM Abrir la página de detalles de Python 3.11 en la Microsoft Store
     start ms-windows-store://pdp/?ProductId=9NRWMJP3717K
-	
+
     echo Por favor, instala Python desde Microsoft Store y luego vuelve a ejecutar este archivo.
     pause > nul
     exit
 )
 
-REM Verificar si speedtest-cli está instalado
+REM Verificar si speedtest cli esta instalado
 speedtest-cli --version >nul 2>&1
+
 if !errorlevel! neq 0 (
     echo speedtest-cli no instalado o correctamente configurado. Verificando...
-	
-    pip install speedtest-cli >nul 2>&1
-	
+
+    pip install speedtest-cli > nul 2>&1
+
     REM Obtener la ruta de speedtest-cli
 	for /d %%i in ("%USERPROFILE%\AppData\Local\Packages\Python*") do (
 		set speedtest_cli_path=%%~i\LocalCache\local-packages\Python311\Scripts 
@@ -34,22 +35,22 @@ if !errorlevel! neq 0 (
 		exit
 	)
 
+    start SystemPropertiesAdvanced.exe
+    echo !speedtest_cli_path! | clip
+
     echo.
     echo Instrucciones de configuracion:
-    echo 1. Abre el menu de inicio y busca "Variables de entorno".
-    echo 2. Selecciona "Editar las variables de entorno del sistema".
-    echo 3. Haz clic en el boton "Variables de entorno".
-    echo 4. En la seccion "Variables del sistema", busca la variable "Path".
-    echo 5. Haz clic en el boton "Editar".
-    echo 6. Agrega la siguiente ruta: 
+    echo 1. Haz clic en el boton "Variables de entorno".
+    echo 2. En la seccion "Variables del sistema", busca la variable "Path".
+    echo 3. Haz clic en el boton "Editar".
+    echo 4. Agrega la siguiente ruta ya copiada en el portapapeles
     echo !speedtest_cli_path!
-    echo 7. Haz clic en "Aceptar" en todas las ventanas para cerrarlas.
+    echo 5. Haz clic en "Aceptar" en todas las ventanas para cerrarlas.
     echo.
-	
+
     echo Una vez realizados todos los pasos, por favor, vuelve a ejecutar este archivo.
 	pause > nul
     exit
 )
-
 
 python medirInternet.py
