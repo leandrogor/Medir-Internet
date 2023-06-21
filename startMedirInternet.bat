d32b60efe746c8149ca57a0432b06bce32123b80
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-06-21 09:31:20 -0300

    Verificar si speedtest-cli está instalado

--- startMedirInternet.bat
+++ startMedirInternet.bat
@@ -7,9 +7,16 @@ if %errorlevel% neq 0 (
     REM Abrir la página de detalles de Python 3.11 en la Microsoft Store
     start ms-windows-store://pdp/?ProductId=9NRWMJP3717K
     echo Por favor, instala Python desde Microsoft Store y luego vuelve a ejecutar este archivo.
-    pause >nul
+    pause > nul
     exit
 )
 
+REM Verificar si speedtest-cli está instalado
+speedtest-cli --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo speedtest-cli no está instalado. Instalando speedtest-cli...
+    pip install speedtest-cli
+)
+
 python medirInternet.py
-pause >nul
+pause > nul
