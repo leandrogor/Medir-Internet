252596bb0d1f66c89aa7eea180451c05df491aa1
Leandro Gabriel Gor <90048504+leandrogor@users.noreply.github.com>
2023-06-21 11:59:33 -0300

    Update startMedirInternet.bat

--- startMedirInternet.bat
+++ startMedirInternet.bat
@@ -1,8 +1,9 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM Verificar si Python está instalado
 python --version >nul 2>&1
-if %errorlevel% neq 0 (
+if !errorlevel! neq 0 (
     echo Python no instalado.
 	
     REM Abrir la página de detalles de Python 3.11 en la Microsoft Store
@@ -16,17 +17,17 @@ if %errorlevel% neq 0 (
 
 REM Verificar si speedtest-cli está instalado
 speedtest-cli --version >nul 2>&1
-if %errorlevel% neq 0 (
+if !errorlevel! neq 0 (
     echo speedtest-cli no instalado o correctamente configurado. Verificando...
 	
     pip install speedtest-cli >nul 2>&1
-
+	
     REM Obtener la ruta de speedtest-cli
 	for /d %%i in ("%USERPROFILE%\AppData\Local\Packages\Python*") do (
-		set speedtest_cli_path=%%~i\LocalCache\local-packages\Python*\Scripts 
+		set speedtest_cli_path=%%~i\LocalCache\local-packages\Python311\Scripts 
 	)
 
-	if %speedtest_cli_path%=="" (
+	if "!speedtest_cli_path!"=="" (
 		echo No se pudo encontrar la ruta de speedtest-cli.
 		echo Por favor, verifica la instalacion y configuracion de speedtest-cli.
 		pause > nul
@@ -34,12 +35,12 @@ if %errorlevel% neq 0 (
 	)
 
 	REM Convertir la ruta a una ruta absoluta
-	pushd "%speedtest_cli_path%"
-	set "speedtest_cli_path=%CD%"
+	pushd "!speedtest_cli_path!"
+	set "speedtest_cli_path=!CD!"
 	popd 
 
 	REM Añadir la ruta al PATH
-	setx PATH "%PATH%;%speedtest_cli_path%" >nul 2>&1
+	setx PATH "%PATH%;!speedtest_cli_path!" >nul 2>&1
 
     echo.
     echo Instrucciones de configuracion:
@@ -48,8 +49,8 @@ if %errorlevel% neq 0 (
     echo 3. Haz clic en el boton "Variables de entorno".
     echo 4. En la seccion "Variables del sistema", busca la variable "Path".
     echo 5. Haz clic en el boton "Editar".
-    echo 6. Agrega la ruta completa al directorio de speedtest-cli: 
-    echo %speedtest_cli_path%
+    echo 6. Agrega la siguiente ruta: 
+    echo !speedtest_cli_path!
     echo 7. Haz clic en "Aceptar" en todas las ventanas para cerrarlas.
     echo.
 	
